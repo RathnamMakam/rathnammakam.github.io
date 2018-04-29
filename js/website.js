a35dd8344fec0af95ed901f6181861549ad1f2db
Rathnam Makam <rathnammakam@gmail.com>
2018-04-29 12:33:27 -0500

    Create home

--- js/website.js
+++ js/website.js
@@ -1,8 +1,8 @@
 angular.module('mywebsite', ['ngRoute']).
     config(function ($routeProvider) {
         $routeProvider.
-            when('/about', {templateUrl: 'partials/about.html'}).
-            when('/home', {templateUrl: 'partials/about.html'}).
+            when('/about', {templateUrl: 'partials/home.html'}).
+            when('/home', {templateUrl: 'partials/home.html'}).
             when('/certifications', {templateUrl: 'partials/certifications.html'}).
             when('/contactme', {templateUrl: 'partials/contactme.html'}).
             otherwise({redirectTo: '/home'});
